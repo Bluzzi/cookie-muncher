5a605a89d5b9d0f7f7b6543f2f2f138aa0e8d713
Bluzzi <camilledugas.pro@gmail.com>
2023-04-23 06:25:43 +0200

    feat(cookie.ts): add Cookie and CookieOptions types for handling cookies

---
+++ src/types/cookie.ts
@@ -0,0 +1,23 @@
+export type Cookie = {
+  name: string;
+  value: string;
+}
+
+/**
+ * @property {number} maxAge - The maximum age of the cookie in seconds
+ * @property {Date} expires - The expiration date of the cookie
+ * @property {string} domain - The domain for which the cookie is valid
+ * @property {string} path - The path for which the cookie is valid
+ * @property {boolean} secure - Whether the cookie should only be sent over HTTPS
+ * @property {boolean} httpOnly - Whether the cookie should only be accessible via HTTP
+ * @property {"Strict" | "Lax" | "None"} sameSite - The same-site policy for the cookie
+ */
+export type CookieOptions = {
+  maxAge?: number;
+  expires?: Date;
+  domain?: string;
+  path?: string;
+  secure?: boolean;
+  httpOnly?: boolean;
+  sameSite?: "Strict" | "Lax" | "None";
+}
