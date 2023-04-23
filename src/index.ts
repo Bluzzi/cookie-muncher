bcc5b33e9ec687324181e457ada54aa8685be846
Bluzzi <camilledugas.pro@gmail.com>
2023-04-23 06:59:13 +0200

    feat(serializer): add CookieMaxAge enum with predefined values for cookie max age

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "#/types/cookie";
 export * from "#/serializer/serializer";
+export * from "#/serializer/serializer.enum";
 export * from "#/parser/parser";
---
+++ src/serializer/serializer.enum.ts
@@ -0,0 +1,12 @@
+export enum CookieMaxAge {
+  OneHour = 60 * 60, // 1 hour
+  SixHours = 6 * 60 * 60, // 6 hours
+  TwlveHours = 12 * 60 * 60, // 12 hours
+  OneDay = 24 * 60 * 60, // 1 day
+  OneWeek = 7 * 24 * 60 * 60, // 1 week
+  TwoWeeks = 14 * 24 * 60 * 60, // 2 weeks
+  OneMonth = 30 * 24 * 60 * 60, // 1 month
+  ThreeMonths = 90 * 24 * 60 * 60, // 3 months
+  SixMonths = 180 * 24 * 60 * 60, // 6 months
+  OneYear = 365 * 24 * 60 * 60, // 1 year
+}
