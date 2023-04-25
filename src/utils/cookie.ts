505b6b01692f2889ae06dc66835af085c3d94d22
Bluzzi <camilledugas.pro@gmail.com>
2023-04-25 22:03:11 +0200

    fix(cookie.ts): remove period from error message to match convention

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,5 +1,5 @@
 export const cookieMaxByteSize = 4096;
 
 export function isCookieEnabled(): void {
-  if (!navigator.cookieEnabled) throw Error("Cookies are disabled by the browser.");
+  if (!navigator.cookieEnabled) throw Error("Cookies are disabled by the browser");
 }
