1ba5cf6c465eab4c3e3e57f3005eeb72f4c446e7
Bluzzi <camilledugas.pro@gmail.com>
2023-04-25 13:45:22 +0200

    refactor(serializer.ts): encode cookie name, value, domain and path using encodeURIComponent()

--- src/http/serializer/serializer.ts
+++ src/http/serializer/serializer.ts
@@ -4,12 +4,12 @@ import type { Cookie, HttpCookieOptions } from "#/typing/cookie";
  * Format a string representing a cookie, useful to be able to set a cookie in a HTTP Set-Cookie header.
  */
 export function serializeCookie(cookie: Cookie, options: HttpCookieOptions = {}): string {
-  const parts: string[] = [`${cookie.name}=${cookie.value}`];
+  const parts: string[] = [`${cookie.name}=${encodeURIComponent(cookie.value)}`];
 
   if (options.maxAge) parts.push(`Max-Age=${options.maxAge}`);
   if (options.expires) parts.push(`Expires=${options.expires.toUTCString()}`);
-  if (options.domain) parts.push(`Domain=${options.domain}`);
-  parts.push(`Path=${options.path || "/"}`);
+  if (options.domain) parts.push(`Domain=${encodeURIComponent(options.domain)}`);
+  parts.push(`Path=${encodeURIComponent(options.path || "/")}`);
   if (options.secure) parts.push("Secure");
   if (options.httpOnly) parts.push("HttpOnly");
   if (options.sameSite) parts.push(`SameSite=${options.sameSite}`);
