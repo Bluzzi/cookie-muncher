5cf15f0630302daf208afc67dee414d090979976
Bluzzi <camilledugas.pro@gmail.com>
2023-04-25 21:30:34 +0200

    feat(string.ts): add getByteSize function to calculate byte size of a string

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,9 @@
 export function capitalizeFirstLetter(content: string): string {
   return `${content.charAt(0).toUpperCase()}${content.slice(1)}`;
 }
+
+export function getByteSize(content: string): number {
+  const encoder = new TextEncoder();
+
+  return encoder.encode(content).byteLength;
+}
