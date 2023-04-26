18b1805e43e8ce0229d713f6aaae4cc8647d4203
Bluzzi <camilledugas.pro@gmail.com>
2023-04-26 22:20:22 +0200

    feat(vitest.config.ts): add jsdom environment for testing with options to set url to https://example.com deps(package.json): add jsdom dependency to package.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    environmentOptions: {
+      jsdom: {
+        url: "https://example.com"
+      }
+    }
+  },
   resolve: {
     alias: {
       "#/": "./src/"
