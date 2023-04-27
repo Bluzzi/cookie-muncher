312603c85d7a5251480b85842da1218c4d91f9be
Bluzzi <camilledugas.pro@gmail.com>
2023-04-27 02:55:59 +0200

    feat(set.ts): add cookieMaxCount constant to limit the number of cookies to 50 fix(set.ts): change error message to use cookieMaxByteSize constant fix(cookie.ts): add cookieMaxCount constant to limit the number of cookies to 50

--- src/dom/set/set.ts
+++ src/dom/set/set.ts
@@ -1,21 +1,21 @@
 import type { Cookie, DomCookieOptions } from "#/typing/cookie";
 import { serializeCookie } from "#/http/serializer";
 import { getByteSize } from "#/utils/string";
-import { cookieMaxByteSize, isCookieEnabled } from "#/utils/cookie";
+import { cookieMaxByteSize, cookieMaxCount, isCookieEnabled } from "#/utils/cookie";
 
 export function setCookie(cookie: Cookie, options: DomCookieOptions = {}): void {
   isCookieEnabled();
 
   const cookiesCount = document.cookie.split(";").length;
 
-  if (cookiesCount > 50) {
-    throw Error("You have more than 50 cookies, most browsers limit the number of cookies to 50");
+  if (cookiesCount >= cookieMaxCount) {
+    throw new Error("You have more than 50 cookies, most browsers limit the number of cookies to 50");
   }
 
   const newCookie = serializeCookie(cookie, options);
 
   if (getByteSize(newCookie) > cookieMaxByteSize) {
-    throw Error(`The size of this cookie is greater than ${cookieMaxByteSize} bytes, most browsers limit the number of cookies to this size.`);
+    throw new Error("The size of this cookie is greater than 4096 bytes, most browsers limit the number of cookies to this size");
   }
 
   document.cookie = serializeCookie(cookie, options);
--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,4 +1,5 @@
 export const cookieMaxByteSize = 4096;
+export const cookieMaxCount = 50;
 
 export function isCookieEnabled(): void {
   if (!navigator.cookieEnabled) throw Error("Cookies are disabled by the browser");
